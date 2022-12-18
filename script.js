8f8e30ee6f041c40024558300fa6363718786c6e
kaykyls <20211bsifsa0025@ifba.edu.br>
2022-12-18 15:55:10 -0300

    almost done

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 const box = document.querySelectorAll(".container div")
-const x = document.querySelectorAll(".container div p:nth-child(1)")
-const circle = document.querySelectorAll(".container div p:nth-child(2)")
 const displayTurn = document.querySelector(".turn p")
 const xWinsScoreboard = document.querySelector(".scoreboard p:nth-child(1)")
 const circleWinsScoreboard = document.querySelector(".scoreboard p:nth-child(2)")
@@ -8,31 +6,33 @@ const playAgainBTn = document.querySelector(".play-again-btn")
 
 console.log(box)
 
-const moves = ["", "", "", "", "", "", "", "", ""]
+let moves = []
 
 let player1 = {
     wins: 0,
-    move: ""
+    move: "X"
 }
 
 let player2 = {
     wins: 0,
-    move: ""
+    move: "O"
 }
 
-let turn = "X"
+let turn = player1.move
+
+let previousFirstTurn
 
 box.forEach((box, index) => {
     box.onclick = () => {
-        if(moves[index] === "") {
+        if(moves[index] === undefined && !checkWin()) {
             const move = document.createElement("p")
             box.append(move)
             move.innerHTML = turn
             moves[index] = turn
             changeTurn()
-            if(checkWin()) {
-                console.log(moves)
-            }
+
+            checkWin()
+            checkDraw()
         }
     }
 })
@@ -43,14 +43,22 @@ const changeTurn = () => {
 
 const checkWin = () => {
     for(let sequence of winSequences) {
-        if(winCondition(moves, sequence)) return true
-        // console.log(sequence)
+        if(winCondition(moves, sequence)) {
+            playAgainBTn.classList.remove("hidden")
+            return true
+        } 
+    }
+}
+
+const checkDraw = () => {
+    if (moves.length === 9) {
+        playAgainBTn.classList.remove("hidden")
     }
 }
 
 const winCondition = (boxesMoves = [], num = []) => {
     // console.log(boxesMoves, num)
-    return boxesMoves[num[0]] === boxesMoves[num[1]] && boxesMoves[num[1]] === boxesMoves[num[2]] && boxesMoves[num[0]] !== ""
+    return boxesMoves[num[0]] === boxesMoves[num[1]] && boxesMoves[num[1]] === boxesMoves[num[2]] && boxesMoves[num[0]] !== undefined
 }
 
 const winSequences = [
@@ -65,3 +73,19 @@ const winSequences = [
     [0, 4, 8],
     [2, 4, 6]
 ]
+
+function resetGame() {
+    box.forEach(box => {
+        if(box.querySelector("p")) {
+            box.querySelector("p").remove()
+        }
+    })
+
+    moves = []
+    playAgainBTn.classList.add("hidden")
+
+    turn = "X"
+    // updateTurnMessage(turn)
+}
+
+playAgainBTn.onclick = resetGame
