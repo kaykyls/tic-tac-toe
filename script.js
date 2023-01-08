987d8a07eca2cc2d8b1446d505480ad97186e525
kaykyls <20211bsifsa0025@ifba.edu.br>
2023-01-08 19:54:51 -0300

    need to fix the checkDraw function

--- script.js
+++ script.js
@@ -4,9 +4,7 @@ const xWinsScoreboard = document.querySelector(".scoreboard p:nth-child(1)")
 const circleWinsScoreboard = document.querySelector(".scoreboard p:nth-child(2)")
 const playAgainBTn = document.querySelector(".play-again-btn")
 
-console.log(box)
-
-let moves = []
+let moves = ["", "", "", "", "", "", "", "", ""]
 
 let player1 = {
     wins: 0,
@@ -24,7 +22,7 @@ let previousFirstTurn
 
 box.forEach((box, index) => {
     box.onclick = () => {
-        if(moves[index] === undefined && !checkWin()) {
+        if(moves[index] === "" && !checkWin() && !checkDraw()) {
             const move = document.createElement("p")
             box.append(move)
             move.innerHTML = turn
@@ -45,20 +43,26 @@ const checkWin = () => {
     for(let sequence of winSequences) {
         if(winCondition(moves, sequence)) {
             playAgainBTn.classList.remove("hidden")
+            console.log("aaaaaaaaa")
             return true
         } 
     }
 }
 
 const checkDraw = () => {
-    if (moves.length === 9) {
-        playAgainBTn.classList.remove("hidden")
-    }
+    moves.forEach(move => {
+        if (move === "") {
+            console.log("aaaa")
+            return false
+        }
+        // playAgainBTn.classList.remove("hidden")
+        return true
+    })
 }
 
 const winCondition = (boxesMoves = [], num = []) => {
     // console.log(boxesMoves, num)
-    return boxesMoves[num[0]] === boxesMoves[num[1]] && boxesMoves[num[1]] === boxesMoves[num[2]] && boxesMoves[num[0]] !== undefined
+    return boxesMoves[num[0]] === boxesMoves[num[1]] && boxesMoves[num[1]] === boxesMoves[num[2]] && boxesMoves[num[0]] !== ""
 }
 
 const winSequences = [
@@ -80,8 +84,7 @@ function resetGame() {
             box.querySelector("p").remove()
         }
     })
-
-    moves = []
+    moves = ["", "", "", "", "", "", "", "", ""]
     playAgainBTn.classList.add("hidden")
 
     turn = "X"
